ede1738fe8aefdf425896e1f988299e59ab613bf
Sydney Keating <sydneykeating2101@gmail.com>
2021-07-27 16:43:11 -0400

    reset

---
+++ app.js
@@ -0,0 +1,11 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+
+
+// start the server listening for requests
