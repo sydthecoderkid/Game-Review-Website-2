7d8dddab6b3441a9912f2787cc63492573b75db0
Sydney Keating <sydneykeating2101@gmail.com>
2021-07-27 16:42:29 -0400

    failed, pt 2

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express()
 app.use(express.static("public"))
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+
 
 // start the server listening for requests
