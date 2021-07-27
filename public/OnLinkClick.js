27097f8f36cbe3ce24386b3499201f8ac2c61473
Sydney Keating <sydneykeating2101@gmail.com>
2021-07-27 12:20:05 -0400

    Finishing Procfile

---
+++ public/OnLinkClick.js
@@ -0,0 +1,14 @@
+function revealInfo(evt, thisID) {
+    // Declare all variables
+    var i, tabcontent;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+       
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(thisID).style.display = "block"
+  }
