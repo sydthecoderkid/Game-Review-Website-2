277c076e49815a115717b5f036009bd15f0750c0
Sydney Keating <sydneykeating2101@gmail.com>
2021-07-27 23:51:27 -0400

    testing

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.static("public"))
 app.get('/index.html', function (req, res) {
   res.type('text/html');
   res.send('<h1>I am html</h1>');
-  console.log("Check");
 })
 
 // start the server listening for requests
