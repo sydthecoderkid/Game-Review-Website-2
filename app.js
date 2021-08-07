ff0079cc7a1fdab2799e9277a0d8e0b71f0aaa34
Sydney Keating <sydneykeating2101@gmail.com>
2021-08-07 17:08:12 -0400

    troubleshooting

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get('/getdata', function (req, res) {
       if (err) throw err;
       con.query("SELECT * FROM GameReviews", function (err, result, fields) {
         if (err) throw err;
+        console.log(data.Name);
         res.send(result[0]);
       });
     });
