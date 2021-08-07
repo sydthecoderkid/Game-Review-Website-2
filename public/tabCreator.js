b37c4e989d34b6b3c4b01537bb120dfb36acb0f0
Sydney Keating <sydneykeating2101@gmail.com>
2021-08-07 10:42:25 -0400

    edited tabcreator

--- public/tabCreator.js
+++ public/tabCreator.js
@@ -3,7 +3,7 @@
 function createTab(name){
     var button = document.createElement('button');
     button.className = 'tablink'
-    button.innerHTML = name;
+    button.innerHTML = "Hello world!";
     button.onclick = function(){
       alert('here be dragons');return false;
     };
