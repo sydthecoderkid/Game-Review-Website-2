ea6aa800cf1e9c22c3337423132de8ab0e4ace99
Sydney Keating <sydneykeating2101@gmail.com>
2021-08-07 20:58:07 -0400

    using res send

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/getdata', function (req, res) {
           if (err) throw err;
            if(result.length > 0){
               for(var i in result){
-                console.log(result[i].Name);
+                res.send[result[i]]
               }
            }
         });
