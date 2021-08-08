be36a3c4acdacbfd3d87d047dd09ad0fadc6c2e9
Sydney Keating <sydneykeating2101@gmail.com>
2021-08-07 20:29:54 -0400

    forgot how arrays worked lmfao

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/getdata', function (req, res) {
           if (err) throw err;
            if(result.length > 0){
              for(i =1; i <result.length; i++){
-                res.sendStatus(i);
+                res.sendStatus(result[i]);
              }
            }
         });
